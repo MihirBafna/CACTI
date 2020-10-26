c13bf0175ae0ba24354cacb92b5539d81e55fd7e
Mihir Bafna <mihir.bafna@gmail.com>
2020-10-26 19:13:19 -0400

    added basic heatmap functionality

--- CACTI_APP/App.js
+++ CACTI_APP/App.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions} from 'react-native';
 import Map from './components/Map'
-import MapView, {Marker} from 'react-native-maps';
+import MapView, {Marker, Heatmap} from 'react-native-maps';
 
 export default function App() {
   return (
@@ -14,13 +14,28 @@ export default function App() {
         longitudeDelta: 0.0421,
       }}>
         <Marker coordinate={{ latitude: 33.776846542110924, longitude: -84.3978524208068 }} />
+        <Heatmap opacity={0.5} radius={30} points={WeightedLatLng}>
+
+        </Heatmap>
       </MapView>      
       <StatusBar style="auto" />
     </View>
   );
 }
 
-
+var WeightedLatLng = [{
+  latitude: 33.7768,
+  longitude: -84.3978,
+  weight: 30,
+}, {
+    latitude: 33.7768,
+    longitude: -84.3978,
+    weight: 30,
+  }, {
+    latitude: 33.568,
+    longitude: -84.3978,
+    weight: 30,
+  }]
 const styles = StyleSheet.create({
   container: {
     flex: 1,
